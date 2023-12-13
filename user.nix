7f1080d1d2bceeeb42694a89dce6d2e45e700965
Mirza Arnaut <21687187+arunoruto@users.noreply.github.com>
2023-12-13 01:23:01 +0100

    Update user.nix

--- user.nix
+++ user.nix
@@ -2,7 +2,7 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "mirza";
-  home.homeDirectory = "/home/mirza";
+  home.username = "user";
+  home.homeDirectory = "/home/user";
 
 }
