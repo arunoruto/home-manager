9b9a0488d449a9f5ebf3980ddf77a99b55273dca
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-06 21:05:39 +0200

    Move fzf with the zsh config to its own file

--- shell/default.nix
+++ shell/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ./bat.nix
+    ./fzf.nix
     ./editorconfig.nix
     ./misc.nix
     ./starship.nix
--- shell/zsh.nix
+++ shell/zsh.nix
@@ -21,18 +21,6 @@
     sessionVariables = {
       PATH = "$HOME/.bin:$PATH";
       TEST = "1234";
-
-      #FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
-      #FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
-      #FZF_ALT_C_COMMAND = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
-      FZF_DEFAULT_OPTS = ''
-        --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
-        --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
-        --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796
-      '';
-
-      FZF_CTRL_T_OPTIONS = "--prview 'bat -n --color=always --line-range :500 {}'";
-      FZF_ALT_C_OPTIONS = "--prview 'eza --tree --color=always {} | head -200'";
     };
     history = {
       size = 10000;
@@ -53,18 +41,6 @@
         echo pr''${PR_NO} = "import (fetchTarball"
         echo "  \"\''${nixpkgs-tars}''${HASH}.tar.gz\")"
         echo "    { config = config.nixpkgs.config; };"
-      }
-
-      _fzf_comprun() {
-        local command=$1
-        shift
-
-        case "$command" in
-          cd)           fzf --preview 'eza --tree --level=2 --color=always {} | head -200' "$@" ;;
-          export|unset) fzf --preview "'eval 'echo \$' {}" "$@" ;;
-          ssh)          fzf --preview 'dig {}' "$@" ;;
-          *)            fzf --preview "--preview 'bat -n --color=always --line-range :500 {}'" "$@" ;;
-        esac
       }
 
       export LS_COLORS="$(vivid generate catppuccin-macchiato)";
