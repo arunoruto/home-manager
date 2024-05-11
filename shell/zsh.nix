62be7911ce2e8ab63227052588559635c9cddc04
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-11 02:48:04 +0200

    Remove some not needed aliases, enable antidote (much faster!) and add tailscale autocomplete

--- shell/zsh.nix
+++ shell/zsh.nix
@@ -3,17 +3,10 @@
     enable = true;
     dotDir = ".config/zsh";
     shellAliases = {
-      #sudo = "sudo -E -s ";
-      #ls = "eza";
-      #ls = "lsd";
-      #ll = "ls -l";
-      #la = "ls -la";
       grep = "rg";
-      #vim = "nvim";
       update = ''bash -c "sudo nixos-rebuild switch"'';
       update-channel = ''bash -c "sudo nix-channel --update"'';
       home = "home-manager switch";
-      #rm-gtk-settings = "rm ~/.config/gtk-4.0/settings.ini ~/.config/gtk-3.0/settings.ini";
       tss = "tailscale switch $(tailscale switch --list | tail -n +2 | fzf | tr -s ' ' | cut -d' ' -f1)";
       tsr = ''bash -c "sudo systemctl restart tailscaled.service"'';
       #tsen = "tailscale status | grep 'offers exit node' | fzf | tr -s ' ' | cut -d' ' -f2";
@@ -27,10 +20,13 @@
       path = "${config.xdg.dataHome}/zsh/history";
     };
     #autosuggestion.enable = true;
-    enableAutosuggestions = true;
-    enableCompletion = true;
+    enableAutosuggestions = false;
+    enableCompletion = false;
 
     initExtra = ''
+      # Enable autocomplete for . and ..
+      zstyle ':completion:*' special-dirs true
+
       bindkey "^[[1;5C" forward-word
       bindkey "^[[1;5D" backward-word
 
@@ -44,31 +40,34 @@
       }
 
       export LS_COLORS="$(vivid generate catppuccin-macchiato)";
+
+      eval "tailscale completion zsh > ~/.config/zsh/_tailscale"
+      source ~/.config/zsh/_tailscale
     '';
-    #antidote = {
-    #  enable = true;
-    #  plugins = [
-    #    "zsh-users/zsh-autosuggestions"
-    #    "zsh-users/zsh-syntax-highlighting"
-    #  ];
-    #};
-    oh-my-zsh = {
+    antidote = {
       enable = true;
-      #  theme = "robbyrussell";
-      #  plugins = [
-      #  	"git"
-      #    "thefuck"
-      #    "docker"
-      #  ];
+      plugins = [
+        "zsh-users/zsh-autosuggestions"
+        "zsh-users/zsh-syntax-highlighting"
+      ];
     };
-    plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = builtins.fetchGit {
-          url = "https://github.com/zsh-users/zsh-autosuggestions";
-          ref = "master";
-        };
-      }
-    ];
+    # oh-my-zsh = {
+    #   enable = true;
+    #   #  theme = "robbyrussell";
+    #   #  plugins = [
+    #   #  	"git"
+    #   #    "thefuck"
+    #   #    "docker"
+    #   #  ];
+    # };
+    # plugins = [
+    #   {
+    #     name = "zsh-autosuggestions";
+    #     src = builtins.fetchGit {
+    #       url = "https://github.com/zsh-users/zsh-autosuggestions";
+    #       ref = "master";
+    #     };
+    #   }
+    # ];
   };
 }
