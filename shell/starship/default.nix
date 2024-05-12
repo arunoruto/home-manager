1bdadeb0dcfb1169d629b5b0601a1df1c40a0ca6
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-12 23:38:23 +0200

    Change starship a bit

--- shell/starship/default.nix
+++ shell/starship/default.nix
@@ -17,10 +17,19 @@ in {
     enable = true;
     settings =
       {
+        format = "$directory$character";
+        # format = "$directory";
+        right_format = "$all";
+        add_newline = true;
         character = {
           success_symbol = "[[󰄛](green) ❯](maroon)";
           error_symbol = "[❯](red)";
           vimcmd_symbol = "[❮](green)";
+        };
+        directory = {
+          truncation_length = 2;
+          truncation_symbol = "..";
+          style = "bold lavender";
         };
         palette = "catppuccin_${flavour}";
       }
