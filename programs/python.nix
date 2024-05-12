387218c7a509d9ef3aac8af1c3009404561a776c
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-12 23:36:40 +0200

    Format python.nix and reorder packages

--- programs/python.nix
+++ programs/python.nix
@@ -1,38 +1,42 @@
-{ pkgs, ... }:
-let
-  packages = ps: with ps; [
-    matplotlib
-    numba
-    numpy
-    opencv4
-    pandas
-    pillow
-    requests
-    scikit-image
-    (
-      buildPythonPackage rec {
-        pname = "streamlit";
-        version = "1.21.0";
-        src = fetchPypi {
-          inherit pname version;
-          sha256 = "sha256-xy2WOVCGecXkEdH4htITd3dZUB0Bl1KFwEncMNtGPBo=";
-        };
-        doCheck = false;
-        propagatedBuildInputs = [
-          # Specify dependencies
-          #pkgs.python3Packages.numpy
-          pkgs.python3Packages.python-dateutil
-          pkgs.python3Packages.click
-          pkgs.python3Packages.pillow
-          pkgs.python3Packages.rich
-          pkgs.python3Packages.pympler
-          pkgs.python3Packages.protobuf
-        ];
-      }
-    )
-  ];
-in
-{
+{pkgs, ...}: let
+  packages = ps:
+    with ps; [
+      # Must
+      matplotlib
+      numba
+      numba-scipy
+      numpy
+      pandas
+      plotly
+      streamlit
+      # Optional
+      # opencv4
+      # pillow
+      # requests
+      # scikit-image
+      # (
+      #   buildPythonPackage rec {
+      #     pname = "streamlit";
+      #     version = "1.21.0";
+      #     src = fetchPypi {
+      #       inherit pname version;
+      #       sha256 = "sha256-xy2WOVCGecXkEdH4htITd3dZUB0Bl1KFwEncMNtGPBo=";
+      #     };
+      #     doCheck = false;
+      #     propagatedBuildInputs = [
+      #       # Specify dependencies
+      #       #pkgs.python3Packages.numpy
+      #       pkgs.python3Packages.python-dateutil
+      #       pkgs.python3Packages.click
+      #       pkgs.python3Packages.pillow
+      #       pkgs.python3Packages.rich
+      #       pkgs.python3Packages.pympler
+      #       pkgs.python3Packages.protobuf
+      #     ];
+      #   }
+      # )
+    ];
+in {
   home.packages = with pkgs; [
     (python3.withPackages packages)
     poetry
