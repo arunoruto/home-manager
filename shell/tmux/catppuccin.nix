dbea1668692be3161c65ba9f9935a46ba389067c
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-21 01:36:09 +0200

    Fix some things about pomodoro

--- shell/tmux/catppuccin.nix
+++ shell/tmux/catppuccin.nix
@@ -1,21 +1,20 @@
-{pkgs, ...}:
-# let
-#   tmux-catpuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
-#     pluginName = "catppuccin";
-#     version = "2024-05-12";
-#     src = pkgs.fetchFromGitHub {
-#       owner = "catppuccin";
-#       repo = "tmux";
-#       rev = "697087f593dae0163e01becf483b192894e69e33";
-#       sha256 = "sha256-EHinWa6Zbpumu+ciwcMo6JIIvYFfWWEKH1lwfyZUNTo=";
-#     };
-#   };
-# in
-{
+{pkgs, ...}: let
+  tmux-catpuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "2024-05-12";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "tmux";
+      rev = "697087f593dae0163e01becf483b192894e69e33";
+      sha256 = "sha256-EHinWa6Zbpumu+ciwcMo6JIIvYFfWWEKH1lwfyZUNTo=";
+    };
+  };
+in {
   programs.tmux = {
     plugins = with pkgs; [
       {
-        plugin = tmuxPlugins.catppuccin;
+        # plugin = tmuxPlugins.catppuccin;
+        plugin = tmux-catpuccin;
         extraConfig = ''
           set -g @catppuccin_flavour "macchiato"
           set -g @catppuccin_custom_plugin_dir "~/.tmux/plugins/tmux/custom"
