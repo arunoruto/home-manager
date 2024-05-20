fdeb5c2933d706b3bdb75bdf933c26f7c324fbd8
arunoruto <mirza.arnaut@tu-dortmund.de>
2024-05-20 20:15:38 +0200

    Formatting and remove some things for stylix

--- gnome/theming.nix
+++ gnome/theming.nix
@@ -1,43 +1,45 @@
-{ pkgs, lib, ...}:
-let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   flavor = "Macchiato";
   accent = "Green";
   bright = "Dark";
   catppuccin_name = "Catppuccin-${flavor}-Standard-${accent}-${lib.strings.toLower bright}";
   catppuccin_path = "Catppuccin-${flavor}-Standard-${accent}-${bright}";
   catppuccin_pkg = pkgs.catppuccin-gtk.override {
-        accents = [ "${lib.strings.toLower accent}" ];
-        size = "standard";
-        tweaks = [ "normal" ];
-        variant = lib.strings.toLower flavor;
+    accents = ["${lib.strings.toLower accent}"];
+    size = "standard";
+    tweaks = ["normal"];
+    variant = lib.strings.toLower flavor;
   };
   candy-icons = builtins.fetchGit {
     url = "https://github.com/EliverLara/candy-icons";
     ref = "master";
   };
-in
-{
+in {
   # gtk settings
   gtk = {
     enable = true;
-    theme = {
-      name = catppuccin_name;
-      package = catppuccin_pkg;
-    };
-    cursorTheme = {
-      name = "Catppuccin-Macchiato-Dark-Cursors";
-      package = pkgs.catppuccin-cursors.macchiatoDark;
-    };
+    # theme = {
+    #   name = catppuccin_name;
+    #   package = catppuccin_pkg;
+    # };
+    # cursorTheme = {
+    #   name = "Catppuccin-Macchiato-Dark-Cursors";
+    #   package = pkgs.catppuccin-cursors.macchiatoDark;
+    # };
   };
 
   #home.sessionVariables.GTK_THEME = catppuccin_name;
   home.file = {
-    ".config/gtk-4.0/assets" = {
-      recursive = true;
-      source = "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/assets";
-    };
-    ".config/gtk-4.0/gtk.css".source =      "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/gtk.css";
-    ".config/gtk-4.0/gtk-dark.css".source = "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/gtk-dark.css";
+    #   ".config/gtk-4.0/assets" = {
+    #     recursive = true;
+    #     source = "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/assets";
+    #   };
+    #   ".config/gtk-4.0/gtk.css".source =      "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/gtk.css";
+    #   ".config/gtk-4.0/gtk-dark.css".source = "${catppuccin_pkg}/share/themes/${catppuccin_path}/gtk-4.0/gtk-dark.css";
     ".local/share/icons/candy-icons" = {
       recursive = true;
       source = "${candy-icons}";
