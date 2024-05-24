2868b52371780a03e6af51afe177a57abb658f8e
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-05-25 01:44:19 +0200

    Add unstable repo for testing newer package versions

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,13 +21,23 @@
 
   outputs = {
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
     nixvim,
     stylix,
     ...
   }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    # pkgs = nixpkgs.legacyPackages.${system};
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        # make unstable packages available via overlay
+        (final: prev: {
+          unstable = nixpkgs-unstable.legacyPackages.${prev.system};
+        })
+      ];
+    };
     user = import ./username.nix;
   in {
     homeConfigurations."${user}" = home-manager.lib.homeManagerConfiguration {
