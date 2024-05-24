bf0295625e0ea08218d3bcec434e8e7881cf75b7
Mirza Arnaut <mirza.arnaut45@gmail.com>
2024-05-25 01:40:33 +0200

    Experiemnt with different LSPs for Python
    Settle on the default one for now

--- shell/helix/languages/python.nix
+++ shell/helix/languages/python.nix
@@ -4,7 +4,7 @@
       language = [
         {
           name = "python";
-          language-servers = ["pyright" "ruff"];
+          # language-servers = ["pyright" "ruff-lsp"];
           auto-format = true;
           formatter = {
             #command = "black";
@@ -15,16 +15,23 @@
       ];
       language-servers = {
         pyright.config.python.analysis.typeCheckingMode = "basic";
-        ruff = {
+        ruff-lsp = {
           command = "ruff-lsp";
           config.settings.args = ["--ignore" "E501"];
         };
+        # pylsp.config.pylsp = {
+        #   plugins.ruff.enabled = true;
+        #   plugins.ruff.formatEnabled = true;
+        # };
       };
     };
     extraPackages = with pkgs; [
       pyright
       ruff
+      ruff-lsp
       black
+      python311Packages.python-lsp-server
+      # python311Packages.python-lsp-ruff
     ];
   };
 }
